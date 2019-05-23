0bc6b813e32be6e5a9c2d9aa403a793a791b964f
dharshanaw <dharshanaw@wso2.com>
2019-05-23 16:18:05 +0530

    Adding info logs for init debug

--- src/main/java/org/wso2/support/sample/samplefederatedAuthenticator/SampleFederatedAuthenticator.java
+++ src/main/java/org/wso2/support/sample/samplefederatedAuthenticator/SampleFederatedAuthenticator.java
@@ -1,5 +1,7 @@
 package org.wso2.support.sample.samplefederatedAuthenticator;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.AbstractApplicationAuthenticator;
 import org.wso2.carbon.identity.application.authentication.framework.FederatedApplicationAuthenticator;
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
@@ -10,28 +12,34 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SampleFederatedAuthenticator extends AbstractApplicationAuthenticator implements
         FederatedApplicationAuthenticator {
+    private static final Log log = LogFactory.getLog(SampleFederatedAuthenticator.class);
+
     @Override
     protected void processAuthenticationResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationContext authenticationContext) throws AuthenticationFailedException {
-
+        log.info("getContextIdentifier");
     }
 
     @Override
     public boolean canHandle(HttpServletRequest httpServletRequest) {
-        return false;
+        log.info("processAuthenticationResponse");
+        return true;
     }
 
     @Override
     public String getContextIdentifier(HttpServletRequest httpServletRequest) {
+        log.info("getContextIdentifier");
         return null;
     }
 
     @Override
     public String getName() {
+        log.info("getName");
         return null;
     }
 
     @Override
     public String getFriendlyName() {
+        log.info("getFriendlyName");
         return null;
     }
 }
